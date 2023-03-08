7b13ae418fe85c781933d23c56a0f20332da09f8
sadsadsadless <vaganov1707@gmail.com>
2023-03-08 11:02:42 +0300

    added fourth lesson

---
+++ hexlet-go/lesson04/practice.go
@@ -0,0 +1,12 @@
+package lesson04
+
+import "fmt"
+
+func PrintName() {
+	// BEGIN (write your solution here)
+	var firstName = "John"
+	var secondName = "Smith"
+
+	fmt.Println(firstName, secondName)
+	// END
+}
