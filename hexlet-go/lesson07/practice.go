9640e7b2b338917689c5e90062393bacd0c92ec2
sadsadsadless <vaganov1707@gmail.com>
2023-03-12 12:09:55 +0300

    added lesson07

---
+++ hexlet-go/lesson07/practice.go
@@ -0,0 +1,8 @@
+package lesson07
+
+// BEGIN (write your solution here)
+func IsValid(id int, text string) bool {
+	return id > 0 && text != ""
+}
+
+// END
---
+++ hexlet-go/lesson07/test.go
@@ -0,0 +1,7 @@
+package lesson07
+
+import "fmt"
+
+func Test() {
+	fmt.Println(IsValid(17, "text"))
+}
